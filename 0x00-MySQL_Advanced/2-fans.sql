3460e199b0b69a15b7f4bc339b7679e960b3ffce
Sadsonmk <sadsonmkumira@yahoo.com>
2024-03-21 08:24:25 +0000

    Adding files

---
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -0,0 +1,6 @@
+-- creates a script that ranks country origins of bands, ordered by the number of (non-unique) fans
+
+SELECT origin, COUNT(fans) AS nb_fans
+FROM metal_bands
+GROUP BY origin
+ORDER BY nb_fans DESC;
