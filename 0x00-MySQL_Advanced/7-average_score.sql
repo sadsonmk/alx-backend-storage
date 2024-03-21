cbb14464d64d5ef04492c0480a2ad732a4d437a2
Sadsonmk <sadsonmkumira@yahoo.com>
2024-03-21 16:42:03 +0000

    Adding files

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -4,9 +4,9 @@
 DELIMITER //
 CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
 BEGIN
-	DECLARE avg_score DECIMAL(5,2);
+	DECLARE avg_score DECIMAL(10,2);
 
-	SELECT AVG(score) INTO avg_score FROM corrections WHERE user_id = user_id;
+	SELECT AVG(score) INTO avg_score FROM scores WHERE user_id = user_id;
 	UPDATE users SET average_score = avg_score WHERE id = user_id;
 END; //
 DELIMITER ;
