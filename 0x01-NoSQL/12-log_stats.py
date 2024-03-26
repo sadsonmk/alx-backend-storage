13068bbf40512c2e3374cf7f9a1f3da292e76813
Sadsonmk <sadsonmkumira@yahoo.com>
2024-03-26 05:30:50 -0700

    Adding the file 12-log

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -2,16 +2,16 @@
 """a module for a Python script that provides
     some stats about Nginx logs stored in MongoDB"""
 
+if __name__ == "__main__":
+    import pymongo
+    client = pymongo.MongoClient("mongodb://127.0.0.1:27017")
 
-import pymongo
-client = pymongo.MongoClient("mongodb://127.0.0.1:27017")
+    val = client.logs.nginx
+    print(f'{val.count_documents({})} logs')
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
 
-value = client.logs.nginx
-print(f'{value.count_documents({})} logs')
-methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-
-print('Methods:')
-for method in methods:
-    print(f'\tmethod {method}: {value.count_documents({"method": method})}')
-print(f'{value.count_documents({"method": "GET", "path": "/status"})}\
+    print('Methods:')
+    for method in methods:
+        print(f'\tmethod {method}: {val.count_documents({"method": method})}')
+    print(f'{val.count_documents({"method": "GET", "path": "/status"})}\
  status check')
